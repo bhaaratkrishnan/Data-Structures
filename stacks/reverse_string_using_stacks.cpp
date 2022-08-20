4a60bcc3fe73f356131ec56e54da8dd884f32666
Bhaarat Krishnan <88877925+bhaaratkrishnan@users.noreply.github.com>
2022-08-20 15:26:37 +0530

    Update reverse_string_using_stacks.cpp

--- stacks/reverse_string_using_stacks.cpp
+++ stacks/reverse_string_using_stacks.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string.h>
-#include<stack>
+#include<stack>//Importing stack data structure from C++ built in library 
 using namespace std;
 void Reverse(char *str, int n){
-    stack<char> S;
+    stack<char> S;//Defining a stack
+    //Pushing elements into stack
     for(int i=0; i<n; i++){
         S.push(str[i]);
     }
+    //Popping a element from a stack to the string in reverse order
+    //as stack is Last in first out data structure(LIFO)
     for(int i=0; i<n; i++){
         str[i]=S.top();
         S.pop();
